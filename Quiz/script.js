f79e737af404e06d6498cddc4d06dd970ff981a9
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-02-02 01:27:53 +0530

    quiz v3

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,4 +1,4 @@
-const questions = [
+const qna = [
   {
     question:
       "What is the time complexity of accessing an element in an array by index?",
@@ -46,3 +46,46 @@ const questions = [
     ],
   },
 ];
+
+questionElement = document.getElementById("question");
+answerElements = document.getElementById("answers");
+nextButton = document.getElementById("next");
+
+let currentIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQnA = qna[currentIndex];
+  let questionNumber = currentIndex + 1;
+  questionElement.innerHTML = questionNumber + ". " + currentQnA.question;
+
+  currentQnA.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerElements.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerElements.firstChild) {
+    answerElements.removeChild(answerElements.firstChild);
+  }
+}
+
+function selectAnswer() {}
+
+startQuiz();
