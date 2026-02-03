d7d87d19d68a1e53deb032b040faf8ca220733c0
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-02-04 02:44:48 +0530

    qr code v2

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -3,8 +3,15 @@ let qrImg = document.getElementById("qr-code");
 let qrTxt = document.getElementById("qr-text");
 
 function generateQR() {
-  qrImg.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrTxt.value;
-  imgSec.classList.add("display-image");
+  if (qrTxt.value.length > 0) {
+    qrImg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrTxt.value;
+    imgSec.classList.add("display-image");
+  } else {
+    qrTxt.classList.add("error");
+    setTimeout(() => {
+      qrTxt.classList.remove("error");
+    }, 200);
+  }
 }
