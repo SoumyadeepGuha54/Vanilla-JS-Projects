164b01701c0c6fcdb9d812a6e3e6f07a75cb3613
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2026-02-04 02:32:19 +0530

    qr code v1

---
+++ QR Code Generator/script.js
@@ -0,0 +1,10 @@
+let imgSec = document.getElementById("image-section");
+let qrImg = document.getElementById("qr-code");
+let qrTxt = document.getElementById("qr-text");
+
+function generateQR() {
+  qrImg.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    qrTxt.value;
+  imgSec.classList.add("display-image");
+}
